f9341e0b0b9e559202fde18cc8689a4a3d871077
nhkim <nhkim@jdlab.kr>
2020-11-09 12:19:04 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import glob
+import pickle
+import numpy as np
+from flask import Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+def init_network():
+    with open("sample_game.pkl", 'rb') as f:
+        network = pickle.load(f)
+    return network
+
+
+@app.route('/')
+def hello_world():
+    return "Hello, World"
+
+
+if __name__ == "__main__":
+    app.run()
